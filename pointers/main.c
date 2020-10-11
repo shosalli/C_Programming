670289fa8b6b6de9f36ded566825937116d8aa53
shosalli <59366089+shosalli@users.noreply.github.com>
2020-10-12 00:13:44 +0530

    Update main.c

--- pointers/main.c
+++ pointers/main.c
@@ -23,16 +23,8 @@ printf("%u\n", &a+2);
 int a[3][3]={{5,7,9},{40,58,36},{42,65,34}};
 
 
-printf("%d\n", a);
-printf("%p\n", a);
-printf("%d\n", *a);
-printf("%d\n", *a+1);
-printf("%d\n", *(a+1));
-
-printf("%u\n", &a);
-printf("%u\n", &a[0]+1);
-printf("%u\n", &a+1);
-printf("%u\n", &a+2);
+printf("%u\n", *(a+1)+2);
+printf("%d\n", *(*(a+1)+2));
     
     
     return 0;
